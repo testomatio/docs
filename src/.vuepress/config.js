2697d0f4f2422f09fdd208625e3ae4f6fa682868
Davert <davert.ua@gmail.com>
2021-11-04 00:22:16 +0200

    fixed crisp integration

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -41,7 +41,7 @@ module.exports = {
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-TVV23D7');`]
+})(window,document,'script','dataLayer','GTM-TVV23D7');`]],
 
   /**
    * Theme configuration, here is the default theme configuration for VuePress.
