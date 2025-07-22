51381b529c1c49e8245fde4fe60efd5fe7f57677
Arhell <arhell333@gmail.com>
2025-07-22 12:15:37 +0300

    fixed the paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -371,6 +371,7 @@ export default defineConfig({
 							{ label: 'Audit Log', link: '/management/company/audit-log' },
 							{ label: 'Trials', link: '/management/company/trials' },
 							{ label: 'Subscriptions', link: '/management/company/subscriptions' },
+							{ label: 'Plan Features Comparison', link: '/management/company/plan-features-comparison' },
 						],
 					},
 						{
