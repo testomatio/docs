b64e73f592bb10e1380b37dd61efa0770925f447
Ihor Sychevskyi <arhell333@gmail.com>
2025-08-12 02:19:55 +0300

    add redirects to reporter pages, fix title & bump dependencies (#425)

--- src/scripts/runok.js
+++ src/scripts/runok.js
@@ -3,7 +3,7 @@ const { runok, tasks: { exec, writeToFile } } = require('runok');
 const axios = require('axios').default;
 const fs = require('fs');
 const path = require('path');
-const humanize = (s) => s.replace(/-([a-z])/g, function (g) { return g[1].toUpperCase()+' '; });
+const humanize = (s) => s.replace(/-([a-z])/g, (_, c) => ' ' + c.toUpperCase()).replace(/^([a-z])/, (_, c) => c.toUpperCase());
 const slugify = require('slugify')
 const dasherize = (str) => slugify(str.toLowerCase());
 const { globSync } = require('glob');
