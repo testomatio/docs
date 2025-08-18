57c602ba134ee0305e00b675c66d024f72a5f598
iaza18 <zyamka1808@gmail.com>
2025-08-18 23:51:37 +0300

    url update

--- astro.config.mjs
+++ astro.config.mjs
@@ -231,7 +231,7 @@ export default defineConfig({
 							label: 'Artifacts',
 							collapsed: true,
 							items: [
-								{ label: 'Artifacts', link: '/advanced/test-artifacts' }
+								{ label: 'Artifacts', link: '/advanced/test-artifacts/artifacts' }
 							],
 						},
 						{
