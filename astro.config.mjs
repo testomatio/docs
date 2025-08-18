526f9c92c0387bd0e4ce4330d10d0b42c4bc9c9b
iaza18 <zyamka1808@gmail.com>
2025-08-18 23:23:32 +0300

    changing redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -230,7 +230,7 @@ export default defineConfig({
 							label: 'Artifacts',
 							collapsed: true,
 							items: [
-								{ label: 'Artifacts', link: '/advanced/artifacts' }
+								{ label: 'Artifacts', link: '/advanced/test-artifacts' }
 							],
 						},
 						{
