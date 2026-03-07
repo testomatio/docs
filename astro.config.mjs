a245eb1d148cb9b95f1c9b89deb26723268c939c
YanaBaranova0909 <qa.yana.baranova@gmail.com>
2026-03-07 14:16:36 +0100

    Mixed run

--- astro.config.mjs
+++ astro.config.mjs
@@ -112,8 +112,9 @@ export default defineConfig({
 							items: [
 								{ label: 'Overview', link: '/project/runs' },
 								{ label: 'Running Tests Manually', link: '/project/runs/running-tests-manually' },
-								{ label: 'Environments', link: '/project/runs/environments' },
 								{ label: 'Running Automated Tests', link: '/project/runs/running-automated-tests' },
+								{ label: 'Running Manual and Automated Tests', link: '/project/runs/running-manual-and-automated-tests' },
+								{ label: 'Environments', link: '/project/runs/environments' },
 								{ label: 'Managing Runs', link: '/project/runs/managing-runs' },
 								{ label: 'RunGroups', link: '/project/runs/rungroups' },
 								{ label: 'Merge Strategies', link: '/project/runs/merge-strategies' },
