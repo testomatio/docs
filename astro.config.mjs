6c1456a030a6e88e3bd8c6a75b56da4489fca464
Kateryna Petrychenko <puckchan04@gmail.com>
2026-07-19 19:33:45 +0300

    moved an astro item to the top according to the comment

--- astro.config.mjs
+++ astro.config.mjs
@@ -87,10 +87,10 @@ export default defineConfig({
 					label: 'Tutorials',
 						items: [
 							{ label: 'Overview', link: '/tutorials'},
+							{ label: 'Manual Testing - BDD', link: '/tutorials/manual-testing-bdd'},
 							{ label: 'Playwright', link: '/tutorials/playwright'},
 							{ label: 'Java Integration', link: '/tutorials/java'},
 							{ label: 'WebdriverIO', link: '/tutorials/webdriver'},
-							{ label: 'Manual Testing - BDD', link: '/tutorials/manual-testing-bdd'},
 						],
        				},
         			{
