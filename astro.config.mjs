0f09c7ac27d8e08a61f4619e697a8101b6b96c5b
Kateryna Petrychenko <puckchan04@gmail.com>
2026-07-19 20:37:08 +0300

    updated toc to fit the code format and proper item order

--- astro.config.mjs
+++ astro.config.mjs
@@ -87,6 +87,11 @@ export default defineConfig({
 					label: 'Tutorials',
 						items: [
 							{ label: 'Overview', link: '/tutorials'},
+							{ label: 'Manual Testing Classic', link: '/tutorials/manual-testing-classic'},
+							{ label: 'Using AI in Testing', link: '/tutorials/using-ai-in-testing'},
+        	   				{ label: 'Working With Jira', link: '/tutorials/working-with-jira'},
+							{ label: 'Set Up Read-Only Access', link: '/tutorials/set-up-read-only-access'},
+							{ label: 'Interacting via API', link: '/tutorials/interacting-via-api'},
 							{ label: 'Migration from TestRail', link: '/tutorials/migration-from-testrail'},
 							{ label: 'Playwright', link: '/tutorials/playwright'},
 							{ label: 'Java Integration', link: '/tutorials/java'},
